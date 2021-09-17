df6c5889dc20f60e5d92785c15e9bde8a445b22a
Umrah <76863190+umrahzargar@users.noreply.github.com>
2021-09-17 18:56:12 +0530

    added intents and updated stories

--- actions/actions.py
+++ actions/actions.py
@@ -527,23 +527,7 @@ class ActionReceiveName(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         text = tracker.latest_message['text']
-        dispatcher.utter_message(text=f"I'll remember your name {text}!")
-        return [SlotSet("name", text)]
+        # dispatcher.utter_message(text=f" {text}!")
+        return [SlotSet("RState", text)]
 
-
-class ActionSayName(Action):
-
-    def name(self) -> Text:
-        return "action_say_name"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        name = tracker.get_slot("name")
-        if not name:
-            dispatcher.utter_message(text="I don't know your name.")
-        else:
-            dispatcher.utter_message(text=f"Your name is {name}!")
-        return []
 
