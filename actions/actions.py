00194403aa99a471a245fb2afe19c1e21e960fe1
IcHiGo-KuRoSaKiI <vedansh21.2000@gmail.com>
2021-09-22 23:17:32 +0530

    Added some stories need more stories

--- actions/actions.py
+++ actions/actions.py
@@ -10,11 +10,10 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import FollowupAction
 import requests
 import re
 from rasa_sdk.forms import FormAction
-
-
 
 ## Preprocessing: defining the state codes for states and UT
 final_states = {'India' : 'TT' , 'Andhra Pradesh': 'AP', 'Arunachal Pradesh': 'AR', 'Assam': 'AS', 'Bihar': 'BR',
@@ -27,18 +26,14 @@ final_states = {'India' : 'TT' , 'Andhra Pradesh': 'AP', 'Arunachal Pradesh': 'A
                 'Delhi': 'DL', 'Ladakh': 'LA', 'Lakshadweep': 'LD', 'Pondicherry': 'PY',
                 'Jammu And Kashmir': 'JK', 'Andaman And Nicobar Islands': 'AN', 'Daman And Diu': 'DD'}
 
-
-
 def getdataFromAPI():
     ## API URL and the API call we need to do 
     url= "https://data.covid19india.org/v4/min/data.min.json"
     r= requests.get(url = url).json()
     return r
 
-
-
 ## Custom function for json preprocessing. This will flattend the json for further steps
-def flatten_json( y):
+def flatten_json(y):
     out = {}
     def flatten(x, name=''):
         # If the Nested key-value
@@ -58,25 +53,27 @@ def flatten_json( y):
     flatten(y)
     return out
 
-
-
-
 class ActionHelloWorld(Action):
-
     def name(self) -> Text:
         return "action_hello_world"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
         print("I am from action file")
         dispatcher.utter_message(text="Hello World response from action.py")
-
         return []
 
 
+# class AskLocation(Action):
+#     def name(self) -> Text:
+#         return "action_ask_location"
 
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         dispatcher.utter_template(tracker, "utter_ask_location")
+#         return [FollowupAction(name="action_listen")]
 
 class Action_corona_stat(Action):
 
@@ -134,10 +131,6 @@ class Action_corona_stat(Action):
         print(message)
         dispatcher.utter_message(message)
 
-
-
-
-
 class Action_Confirmed_stat(Action):
     def name(self) -> Text:
         return "action_confirmed_stat"
@@ -147,13 +140,13 @@ class Action_Confirmed_stat(Action):
         res = ""
         for i in flattedDict.keys ():
             if (state == False and len (re.findall(r"(.+?){fname}_total_confirmed".format(fname=entity_from_chatbot),i)) != 0 ):
-                res += "" + entity_from_chatbot + " : " +  str(flattedDict[i]) + "\n"
+                res +=   str(flattedDict[i]) + "\n"
             ##to get the the total values of only states excluding district
             if ( state == True and len (re.findall(r"{fname}_total_confirmed".format(fname=entity_from_chatbot),i)) != 0):
-                res += "" + entity_from_chatbot + " : "  + str(flattedDict[i]) + "\n" 
+                res +=   str(flattedDict[i]) + "\n" 
         if res == "":
             res += "Data is not available"
-            
+
         return res
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
@@ -173,6 +166,9 @@ class Action_Confirmed_stat(Action):
             elif ( next(tracker.get_latest_entity_values('GPE'), None) != None):
                 entity_from_chatbot = next(tracker.get_latest_entity_values('GPE'), None)
             print ("State to be processed : ", entity_from_chatbot )
+            
+            text = tracker.latest_message['text']
+            print ( text )
         except Exception as e:
             print ( "did not got anything")
             return []
@@ -186,18 +182,18 @@ class Action_Confirmed_stat(Action):
         ## This if computes  if the given input is a state or not and searches accordingly
         try:
             if ( entity_from_chatbot.title() in final_states):
-                message = self.get_sats(final_states[entity_from_chatbot.title().strip()] , flattedDict , True).replace( final_states[entity_from_chatbot.title()] , entity_from_chatbot.title() )
+                # message = self.get_sats(final_states[entity_from_chatbot.title().strip()] , flattedDict , True).replace( final_states[entity_from_chatbot.title()] , entity_from_chatbot.title() )
+                message = self.get_sats(final_states[entity_from_chatbot.title().strip()] , flattedDict , True)
             else:
                 message = self.get_sats(entity_from_chatbot.title().strip() , flattedDict )
-                message = message.replace("districts" , "")
+                # message = message.replace("districts" , "")
+            print(message)
+            dispatcher.utter_message(message) 
+            return []    
         except Exception as e:
             print ( e)
             dispatcher.utter_message("Sorry we don't have information regarding that place")
         ## giving the output to the chatbot 
-        print(message)
-        dispatcher.utter_message(message) 
-        return []       
-
 
 
 
@@ -261,8 +257,6 @@ class Action_Death_stat(Action):
         return []
 
 
-
-
 class Action_Recovered_stat(Action):
     def name(self) -> Text:
         return "action_recovered_stat"
@@ -320,9 +314,6 @@ class Action_Recovered_stat(Action):
         ## giving the output to the chatbot 
         print(message)
         dispatcher.utter_message(message) 
-
-
-
 
 
 class Action_Tested_stat(Action):
@@ -384,9 +375,6 @@ class Action_Tested_stat(Action):
         dispatcher.utter_message(message) 
 
 
-
-
-
 class Action_Vaccine_stat(Action):
     def name(self) -> Text:
         return "action_vaccine_stat"
@@ -445,9 +433,6 @@ class Action_Vaccine_stat(Action):
         ## giving the output to the chatbot 
         print(message)
         dispatcher.utter_message(message)
-
-
-
 
 
 class Action_Delta_stat(Action):
@@ -516,13 +501,6 @@ class Action_Delta_stat(Action):
         ## giving the output to the chatbot 
         print(message)
         dispatcher.utter_message(message)
-
-
-
-
-
-
-
 
 
 # class ActionReceiveName(Action):
